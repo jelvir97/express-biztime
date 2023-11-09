c6052e18c5c53c816de09afaa9cb865d8dd5ba25
Juan Elvir <jelvir97@gmail.com>
2023-11-09 16:31:54 -0700

    PUT /companies/:code'

--- routes/companies.js
+++ routes/companies.js
@@ -33,4 +33,18 @@ router.post('/', async (req,res,next) =>{
     }
 })
 
+router.put('/:code', async (req, res, next) => {
+    try {
+      const { code } = req.params;
+      const { name, description } = req.body;
+      const results = await db.query('UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description', [name, description, code])
+      if (results.rows.length === 0) {
+        throw new ExpressError(`Can't update company with id of ${id}`, 404)
+      }
+      return res.send({ company: results.rows[0] })
+    } catch (e) {
+      return next(e)
+    }
+  })
+
 module.exports = router;
