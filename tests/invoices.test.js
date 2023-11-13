52e306f82fc4f88ae6b9672bc85c8befbe704dbb
Juan Elvir <jelvir97@gmail.com>
2023-11-13 13:25:33 -0700

    test GET /invoices

--- tests/invoices.test.js
+++ tests/invoices.test.js
@@ -5,20 +5,39 @@ const app = require('../app')
 const db = require('../db')
 
 let test_comp;
+let test_inv;
 
 beforeEach(async ()=>{
     const result = await db.query(`INSERT INTO
                                     companies (code, name, description)
                                     VALUES ('abc', 'ABC', 'alphabet')
                                     RETURNING code, name, description`)
+
     test_comp = result.rows[0]
+
+    const invoice = await db.query(`INSERT INTO
+                                    invoices ( comp_code, amt )
+                                    VALUES ('abc',4.50)
+                                    RETURNING id, comp_code, amt `)
+
+    test_inv = invoice.rows[0]
 
 })
 
 afterEach(async()=>{
-    await db.query('DELETE FROM invoices; DELETE FROM invoices')
+    await db.query('DELETE FROM companies')
+    await db.query('DELETE FROM invoices')
 })
 
 afterAll(async ()=>{
     await db.end();
+})
+
+describe('GET /invoices', ()=>{
+    test('returns list of invoices', async ()=>{
+        const resp = await request(app).get('/invoices')
+
+        expect(resp.body).toEqual({invoices: [{id:test_inv.id,comp_code:'abc'}]})
+        expect(resp.statusCode).toEqual(200)
+    })
 })
