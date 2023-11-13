592d9b8eacee83dccae62c91c91641fc325d1491
Juan Elvir <jelvir97@gmail.com>
2023-11-13 16:49:46 -0700

    POST /industries/:ind_code - for ind-comp association

--- routes/industries.js
+++ routes/industries.js
@@ -37,4 +37,18 @@ router.post('/',async (req,res,next)=>{
     }
 })
 
+router.post('/:ind_code',async (req,res,next)=>{
+    try{
+        const {ind_code} = req.params
+        const {comp_code} = req.body
+
+        const results = await db.query('INSERT INTO ind_comp (ind_code, comp_code) VALUES ( $1, $2 ) RETURNING * ',[ind_code,comp_code])
+
+        return res.json(results.rows[0])
+
+    }catch(e){
+        next(e)
+    }
+})
+
 module.exports = router
