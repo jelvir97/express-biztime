4617af2b115c0862fea5885415cd8f42b26207d1
Juan Elvir <jelvir97@gmail.com>
2023-11-13 10:23:51 -0700

    test for GET /companies/:id and POST /companies

--- tests/companies.test.js
+++ tests/companies.test.js
@@ -28,6 +28,36 @@ describe('GET /companies route', ()=>{
     })
 })
 
+describe('GET /companies/:id route', ()=>{
+    test('returns one company with code "abc"',async ()=>{
+        const resp = await request(app).get('/companies/abc')
+
+        expect(resp.statusCode).toEqual(200)
+        expect(resp.body).toEqual({company: { code: 'abc', name: 'ABC', description: 'alphabet', invoices: {}}})
+    })
+
+    test('throws error not found with invalid company code', async ()=>{
+        const resp = await request(app).get('/companies/acb')
+        expect(resp.statusCode).toEqual(404)
+        expect(resp.body).toEqual({"error": {"message": "Company code acb not found", "status": 404}, "message": "Company code acb not found"})
+    })
+})
+
+describe('POST /companies route', ()=>{
+    test('inserts and returns company', async ()=>{
+        const resp = await request(app).post('/companies').send({name:"IBM",code:"ibm",description:"We make stuff"})
+
+        expect(resp.statusCode).toEqual(201)
+        expect(resp.body).toEqual({company: {name:"IBM",code:"ibm",description:"We make stuff"}})
+    })
+
+    test('fails insert due to invalid request',async ()=>{
+        // No handling for this error but tested for internal error
+        const resp = await request(app).post('/companies').send({name:"IBM",description:"We make stuff"})
+        expected(resp.statusCode).toEqual(500)
+    })
+})
+
 afterAll(async ()=>{
     await db.end();
 })
