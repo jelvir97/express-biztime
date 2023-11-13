eee34b2d4110cbb5b983492b9b6634161213873e
Juan Elvir <jelvir97@gmail.com>
2023-11-13 16:31:31 -0700

    GET /industries lists companies

--- routes/industries.js
+++ routes/industries.js
@@ -5,8 +5,20 @@ const db = require("../db");
 
 router.get('/',async (req,res,next)=>{
     try{
-        const results = await db.query('SELECT * FROM industries')
-        return res.json({industries: results.rows})
+        const results = await db.query(`SELECT i.code, i.industry,c.name FROM industries AS i
+                                        LEFT JOIN ind_comp AS ic ON i.code = ic.ind_code
+                                        LEFT JOIN companies AS c on ic.comp_code=c.code`)
+        let obj = {};
+
+        for(let r of results.rows){
+            let key = r.industry
+            if(key in obj){
+                obj[key].companies.push(r.name)
+            } else{
+                obj[key] = {code: r.code, companies:[r.name]}
+            }
+        }
+        return res.json({industries: obj})
     }catch(e){
         next(e)
     }
